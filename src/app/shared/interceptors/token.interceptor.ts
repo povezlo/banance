1d0c7e9c43b1a1ce4b2e750ff91e00cf98e077c7
Andrey <aliester.crowly@gmail.com>
2023-08-28 16:22:54 +0300

    4 feat create structure (#6)
    * 2-feat(Ng app): Rework eslint, github actions [WIP]
    Closes #2
    
    * 3-feat(Init App): Setup ngrx
    
    * 3-feat(Init App): Setup Angular Material
    
    * 4-feat(Init App): Setup base structure [WIP]

---
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -0,0 +1,18 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpInterceptor, HttpHeaders } from '@angular/common/http';
+import { API_KEY } from '../injectTokens/injectTokens';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(@Inject(API_KEY) private readonly apiKey: string) {}
+
+  intercept(request: HttpRequest<HttpHeaders>, next: HttpHandler) {
+    request = request.clone({
+      setParams: {
+        apiKey: this.apiKey,
+      },
+    });
+
+    return next.handle(request);
+  }
+}
